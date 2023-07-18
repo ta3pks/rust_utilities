b3a399b3fe398efb80b00157f5d45a5afc215cb4
nikos efthias <nikos@mugsoft.io>
2023-07-18 15:09:15 +0300

    impl duration for all other numeric types

--- src/time.rs
+++ src/time.rs
@@ -1,10 +1,30 @@
-pub trait Duration {
+macro_rules! impl_for{
+    ($($t:ty),*) => {
+        $(
+        impl DurationExt for $t {
+            fn millis(&self) -> std::time::Duration {
+                (*self as u64).millis()
+            }
+            fn seconds(&self) -> std::time::Duration {
+                (*self as u64).seconds()
+            }
+            fn minutes(&self) -> std::time::Duration {
+                (*self as u64).minutes()
+            }
+            fn hours(&self) -> std::time::Duration {
+                (*self as u64).hours()
+            }
+        }
+    )*
+    };
+}
+pub trait DurationExt {
     fn millis(&self) -> std::time::Duration;
     fn seconds(&self) -> std::time::Duration;
     fn minutes(&self) -> std::time::Duration;
     fn hours(&self) -> std::time::Duration;
 }
-impl Duration for u64 {
+impl DurationExt for u64 {
     fn millis(&self) -> std::time::Duration {
         std::time::Duration::from_millis(*self)
     }
@@ -18,3 +38,5 @@ impl Duration for u64 {
         std::time::Duration::from_secs(self.minutes().as_secs() * 60)
     }
 }
+
+impl_for!(u8, u16, u32, usize, i8, i16, i32, i64, isize, f64, f32);
