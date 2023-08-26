7d53b90cd9f779d0b31be475fb976629a8029af5
nikos efthias <nikos@mugsoft.io>
2023-08-26 15:49:55 +0300

    bugfixes

--- src/json.rs
+++ src/json.rs
@@ -2,6 +2,20 @@ pub trait JsonEncodeExt
 where
     Self: serde::Serialize,
 {
+    fn to_json(&self) -> Result<serde_json::Value, serde_json::Error>;
+    fn to_json_string(&self) -> Result<String, serde_json::Error>;
+    fn to_json_pretty_string(&self) -> Result<String, serde_json::Error>;
+}
+pub trait JsonDecodeExt {
+    fn parse_json<T>(&self) -> Result<T, serde_json::Error>
+    where
+        T: serde::de::DeserializeOwned,
+        Self: AsRef<[u8]>;
+}
+impl<T> JsonEncodeExt for T
+where
+    T: serde::Serialize,
+{
     fn to_json(&self) -> Result<serde_json::Value, serde_json::Error> {
         serde_json::to_value(self)
     }
@@ -12,11 +26,13 @@ where
         serde_json::to_string_pretty(self)
     }
 }
-pub trait JsonDecodeExt {
-    fn parse_json<T>(&self) -> Result<T, serde_json::Error>
+impl<T> JsonDecodeExt for T
+where
+    T: AsRef<[u8]>,
+{
+    fn parse_json<U>(&self) -> Result<U, serde_json::Error>
     where
-        T: serde::de::DeserializeOwned,
-        Self: AsRef<[u8]>,
+        U: serde::de::DeserializeOwned,
     {
         serde_json::from_slice(self.as_ref())
     }
